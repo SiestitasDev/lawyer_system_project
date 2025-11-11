91786ddc344464d07bad938cdaac9658e83f9e37
deyvidsdev <117693564+deyvidsdev@users.noreply.github.com>
2025-11-11 01:16:37 -0500

    Comentando cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
